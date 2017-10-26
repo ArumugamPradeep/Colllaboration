1682afec9e5d054f59d9e592e62c40ee4ac6ab04
Pradeep <pradeepugamathi@gmail.com>
2017-10-26 09:26:21 +0530

    jobservice

--- CollaborationBackend/src/main/java/com/niit/collaborate/service/JobServiceImpl.java
+++ CollaborationBackend/src/main/java/com/niit/collaborate/service/JobServiceImpl.java
@@ -2,26 +2,34 @@ package com.niit.collaborate.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.niit.collaborate.Dao.JobDAO;
 import com.niit.collaborate.Model.Job;
+
+@Service
 
 public class JobServiceImpl implements JobService {
 
+	@Autowired
+	private JobDAO jobDAO;
+
 	@Override
 	public void addJob(Job job) {
-		// TODO Auto-generated method stub
+		jobDAO.addJob(job);
 
 	}
 
 	@Override
 	public List<Job> getAllJobs() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return jobDAO.getAllJobs();
 	}
 
 	@Override
 	public Job getJob(int jobId) {
-		// TODO Auto-generated method stub
-		return null;
+		return jobDAO.getJob(jobId);
 	}
 
 }
